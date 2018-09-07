dca6c459bd04728ed2a265bae9fe62ab95ceeca3
jiandong-tohka <1937636639@qq.com>
2018-09-07 21:20:28 +0800

    hello

---
+++ CG1/CG1/源.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include "glut.h"
+using namespace std;
+
+GLfloat mat[4] = { 0.5, 0.5, 0.9, 0 };
+//π‚‘¥Œª÷√
+GLfloat position[] = { 1.0, 1.0, 5.0, 0.0 };
+static int count_number = 0;
+
+void init(void)
+{
+	GLfloat ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	
+	GLfloat lmodel_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat local_view[] = { 0.0 };
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+	glLightModelfv(GL_LIGHT_MODEL_LOCAL_VIEWER, local_view);
+
+	glFrontFace(GL_CW);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_DEPTH_TEST);
+}
+
+
+void display(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	glTranslatef(8, 8, 0.0);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat);
+	glutSolidTeapot(3.0);
+	glPopMatrix();
+	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w <= h)
+		glOrtho(0.0, 16.0, 0.0, 16.0*(GLfloat)h / (GLfloat)w,
+			-10.0, 10.0);
+	else
+		glOrtho(0.0, 16.0*(GLfloat)w / (GLfloat)h, 0.0, 16.0,
+			-10.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		case 27:
+		{
+			exit(0);
+			break;
+		}
+		// up
+		case 'w':
+		{
+			position[1] += 0.5;
+			init();
+			display();
+			break;
+		}
+		// down
+		case 's':
+		{
+			position[1] -= 0.5;
+			init();
+			display();
+			break;
+		}
+		// left
+		case 'a':
+		{
+			position[0] -= 0.5;
+			init();
+			display();
+			break;
+		}
+		// right
+		case 'd':
+		{
+			position[0] += 0.5;
+			init();
+			display();
+			break;
+		}
+		case 'b':
+		{
+			position[0] = 1.0;
+			position[1] = 1.0;
+			init();
+			display();
+			break;
+		}
+		case 99:
+		{
+
+			if (count_number % 3 == 0)
+			{
+				mat[0] = 1.0;
+				mat[1] = 0;
+				mat[2] = 0;
+				display();
+				count_number++;
+				break;
+			}
+			if (count_number % 3 == 1)
+			{
+				mat[0] = 0;
+				mat[1] = 1.0;
+				mat[2] = 0;
+				display();
+				count_number++;
+				break;
+			}
+			if (count_number % 3 == 2)
+			{
+				mat[0] = 0.5;
+				mat[1] = 0.5;
+				mat[2] = 0.9;
+				display();
+				count_number++;
+				break;
+			}
+		}
+		default:
+		{
+			cout << "Unhandled key press " << key << endl;
+			break;
+		}
+	}
+}
+
+/*
+* Main Loop
+*/
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(150, 150);
+	glutCreateWindow("hello world");
+	init();
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
+}
